0d2e9fd1322b41ae3d57580de8d0e7db6b41d8c8
fc56943 <fc56943@alunos.ciencias.ulisboa.pt>
2024-10-30 17:07:24 +0100

    shows current date

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        try {
+            showdate();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -27,21 +37,34 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void toggleColour (View v){
-        int id = v.getId();
-        v.setBackgroundColor(0xFF009688);
+    public String getDate () throws Exception{
+
+        Calendar cal = Calendar.getInstance();
+        Date today = cal.getTime();
+        return today.toString();
     }
 
-    public void showName (View v){
-        //V -> Button !!!!
+    public void showdate () throws Exception {
 
-        EditText editText = findViewById(R.id.editTextText);
-        String name = editText.getText().toString();
-
-        TextView hello = findViewById(R.id.textView);
-        String newHello = "Hello, " + name;
-        hello.setText(newHello);
-
-        v.setBackgroundColor(0xFF009688);
+        TextView  textView2 = findViewById(R.id.textView2);
+        String today = getDate();
+        textView2.setText(today);
     }
+
+//    public void toggleColour (View v){
+//        int id = v.getId();
+//        v.setBackgroundColor(0xFF009688);
+//    }
+
+//    public void showName (View v){
+//        //V -> Button !!!!
+//        EditText editText = findViewById(R.id.editTextText);
+//        String name = editText.getText().toString();
+//
+//        TextView hello = findViewById(R.id.textView);
+//        String newHello = "Hello, " + name;
+//        hello.setText(newHello);
+//
+//        v.setBackgroundColor(0xFF009688);
+//    }
 }
